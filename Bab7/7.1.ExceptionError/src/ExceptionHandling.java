c90d0a08c08d376905f9dbb86e3c39cece5022b9
rizkisadewa <rizkykysadewa@gmail.com>
2019-08-02 07:09:17 +0800

    working on Exception and Error Handling, but still error

---
+++ Bab7/7.1.ExceptionError/src/ExceptionHandling.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class ExceptionHandling {
+
+    public static double exceptionIf(int a, int b){
+        if(b != 0){
+            return a / b;
+        } else {
+            System.out.print("c hasil exception if: ");
+            return 0;
+        }
+    }
+
+    public static double exceptionTryCatch(int a, int b){
+        try{
+            return a / b;
+        } catch(ArithmeticException e){
+            System.out.print("c hasil exception try catch: ");
+            return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        int a, b;
+
+        Scanner input = new Scanner(System.in);
+
+        a = input.nextInt();
+        b = input.nextInt();
+
+        System.out.println(exceptionIf(a, b));
+        System.out.println(exceptionTryCatch(a, b));
+    }
+
+}
