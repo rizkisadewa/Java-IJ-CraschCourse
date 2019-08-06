31365284b6b4618b2e9afc6b5d50e0f81c4ad1a7
rizkisadewa <rizkykysadewa@gmail.com>
2019-08-06 23:07:49 +0800

    Operasi String , Exception Null Exception

---
+++ Bab7/7.5.NullPointerException/src/NullPointerException.java
@@ -0,0 +1,11 @@
+public class NullPointerException extends RuntimeException{
+    private static final long serialVersionUID = 516271083389028792L;
+
+    public NullPointerException(){
+        super();
+    }
+
+    public NullPointerException(String s){
+        super(s);
+    }
+}
