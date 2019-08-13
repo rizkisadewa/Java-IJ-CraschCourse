c6876da8bc990973e00d4fb92315537655a043a9
rizkisadewa <rizkykysadewa@gmail.com>
2019-08-13 23:22:11 +0800

    IO Exception

---
+++ Bab7/7.8.ClassCastException/src/ClassCastException.java
@@ -0,0 +1,9 @@
+public class ClassCastException extends RuntimeException {
+    private static final long serialVersionUID = -9223365651070458532L;
+    public ClassCastException(){
+        super();
+    }
+    public ClassCastException(String s){
+        super(s);
+    }
+}
