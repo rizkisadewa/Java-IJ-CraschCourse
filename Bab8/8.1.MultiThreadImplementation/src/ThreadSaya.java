d102421a63b24b1e0c15f9a7deadc2d3939c9d9e
rizkisadewa <rizkykysadewa@gmail.com>
2019-08-22 22:44:50 +0800

    Implementation of multithread

---
+++ Bab8/8.1.MultiThreadImplementation/src/ThreadSaya.java
@@ -0,0 +1,33 @@
+public class ThreadSaya implements Runnable{
+    private Thread thread;
+    private String namaThread;
+
+    public ThreadSaya(String namaThread){
+        this.namaThread = namaThread;
+        System.out.println("Thread "+namaThread+" diciptakan");
+    }
+
+    @Override
+    public void run(){
+        System.out.println("Thread "+namaThread+" dijalankan");
+
+        try{
+            for(int i = 10; i>0; i--){
+                Thread.sleep(50);
+            }
+        } catch(InterruptedException e){
+            System.out.println("Thread "+namaThread+" diinterupsi");
+        }
+
+        System.out.println("Thread "+namaThread+" selesai.");
+    }
+
+    public void start(){
+        System.out.println("Thread "+namaThread+" dimulai");
+
+        if(thread == null){ // belum diinstansiasi
+            thread = new Thread(this, namaThread);
+            thread.start();
+        }
+    }
+}
